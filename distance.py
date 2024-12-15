7ae41c9c352a8e1d5bfe13d1dd84a544836edbd8
Arden <121777799+ardenBarden@users.noreply.github.com>
2024-12-15 20:05:48 +0000

    results saved in addressesToCater.csv

--- distance.py
+++ distance.py
@@ -1,1 +1,12 @@
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv('TemplateCopy.csv')
+print(data.columns)
+#['End Address', 'Driving Distance (meters)', 'Driving Distance (miles)']
+cleanedData = data[data['Driving Distance (miles)'] < 5]
+result = cleanedData['End Address']
+print(result)
+
+result.to_csv('addressesToCater.csv', index = None)
 
